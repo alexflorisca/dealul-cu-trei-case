c3478c72b2b9dee0e48f2f7e820da5b2dac835be
Alex Florisca <alexflorisca@gmail.com>
2021-10-16 14:17:37 +0100

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,47 @@
 module.exports = {
-  siteMetadata: {
-    title: `Dealul Cu Trei Case`,
-    description: `Come and experinece the paradise of the Apuseni Mountains`,
-    author: `Florisca Family`,
-    siteUrl: `https://dealulcutreicase.ro/`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /\.inline\.svg$/
-        }
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: `Dealul Cu Trei Case`,
+		description: `Come and experinece the paradise of the Apuseni Mountains`,
+		author: `Florisca Family`,
+		siteUrl: `https://dealulcutreicase.ro/`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-image`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Dealul Cu Trei Case`,
+				short_name: `d3c`,
+				start_url: `/`,
+				background_color: `#399C47`,
+				// This will impact how browsers show your PWA/website
+				// https://css-tricks.com/meta-theme-color-and-trickery/
+				// theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/assets/favicon.svg`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-react-svg',
+			options: {
+				rule: {
+					include: /\.inline\.svg$/,
+				},
+			},
+		},
+
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
+};
