dacf2ce327b668dc801dabf361e817766090e049
Alex Florisca <alexflorisca@gmail.com>
2021-10-22 12:21:54 +0100

    Google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,24 @@ module.exports = {
 				},
 			},
 		},
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				trackingIds: ['G-43ND5HD0CF'],
+				// This object gets passed directly to the gtag config command
+				// This config will be shared across all trackingIds
+				gtagConfig: {
+					optimize_id: 'OPT_CONTAINER_ID',
+					anonymize_ip: true,
+					cookie_expires: 0,
+				},
+				// This object is used for configuration specific to this plugin
+				pluginConfig: {
+					// Puts tracking script in the head instead of the body
+					head: true,
+				},
+			},
+		},
 
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
